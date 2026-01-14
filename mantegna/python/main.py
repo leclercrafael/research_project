250a90111616d41fdeb4620fa254b783b979608a
Rafaël Leclerc <rafael.leclerc.rl@gmail.com>
2026-01-14 20:20:15 +0100

    main

---
+++ mantegna/python/main.py
@@ -0,0 +1,8 @@
+from mantegna import Mantegna
+from data_getter import Data_Getter
+
+
+if __name__ == "__main__":
+    mantegna = Mantegna()
+    mantegna.plot_static_mantegna()
+    mantegna.plot_cluster_mantegna()
